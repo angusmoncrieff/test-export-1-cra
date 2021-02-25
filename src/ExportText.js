9bbeb4fd8ea21ca3b0c7eff6d543ddc445fde2fb
Angus Moncrieff <angus.moncrieff@gmail.com>
2021-02-25 14:18:51 +0000

    ,

---
+++ src/ExportText.js
@@ -0,0 +1,15 @@
+
+const TestComponent = () => {
+    return (
+        <div style={{
+            backgroundColor: 'orange',
+            color: 'darkRed',
+            padding: '20px',
+            fontSize: '30px'
+        }}>
+            THIS IS CPT EXPORTED FROM export-1-cra
+        </div>
+    );
+}
+
+export default TestComponent;
